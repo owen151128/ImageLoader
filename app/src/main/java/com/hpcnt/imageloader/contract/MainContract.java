aaba5c79bdee0ad7012531a90e8c9fe8900d2522
owen151128 <owen151128@gmail.com>
2017-07-28 12:01:01 +0900

    각종 라이브러리 추가, MVP 기본 틀 작성, getHtml 구현

---
+++ app/src/main/java/com/hpcnt/imageloader/contract/MainContract.java
@@ -0,0 +1,24 @@
+package com.hpcnt.imageloader.contract;
+
+import com.android.volley.RequestQueue;
+
+/**
+ * Created by 0wen151128 on 2017. 7. 28..
+ */
+
+public interface MainContract {
+
+    interface View {
+        void setTextView(String text);
+    }
+
+    interface Presenter {
+        void attachView(MainContract.View view);
+
+        void detachView();
+
+        void setVolleyQueue(RequestQueue requestQueue);
+
+        void getHtml(String targetUrl);
+    }
+}
